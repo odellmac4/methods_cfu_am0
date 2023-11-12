b4c2c04e93ea3fed2be44e152d422c1d2f977aff
Odell McFarland IV <odell.mac4@gmail.com>
2023-11-12 12:30:09 -0800

    This commit will create methods for general and custom holiday greetings

--- final_practice.rb
+++ final_practice.rb
@@ -5,23 +5,48 @@
 
 
 # 1: Write a method named greeting that returns a string with a general greeting. 
+def greeting
+    "Greetngs and happy holidays!"
+end
+
+puts greeting
 
 # What is the return value of your method?
+# The return value of the method above is "Greetings and happy holidays!".
 # How many arguments did you pass your method?
+# I don't believe any arguments were passed unless we consider the string to be an argument. My final answer is 0 arguments were passed.
 
 
 # 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
 
+def custom_greeting(name)
+    "Greetings and happy holidays, #{name}!"
+end
+
+puts custom_greeting("Odell")
+
 # What is the return value of your method?
+# Greetings and happy holidays, Odell!
 # How many arguments did you pass your method?
+# One argument was passed
 # What data type was your argument(s)?
+# The data type was a string
 
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
 
+def greet_person(first_name, middle_name, last_name)
+    "Happy Holidays, #{first_name} #{middle_name} #{last_name}! Claim your holiday gift below."
+end
+
+puts greet_person("Robyn", "Caresha", "Combs")
+
 # What is the return value of your method?
+# Happy Holidays, Robyn Caresha Combs! Claim your holiday gift below.
 # How many arguments did you pass your method?
+# Three arguments were passed
 # What data type was your argument(s)?
+# All three data types are strings
 
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
@@ -35,14 +60,14 @@
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
 # Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
 
-check_stock(4, "Coffee");
-# => "Coffee is stocked"
+# check_stock(4, "Coffee");
+# # => "Coffee is stocked"
 
-check_stock(3, "Tortillas");
-# => "Tortillas - running LOW"
+# check_stock(3, "Tortillas");
+# # => "Tortillas - running LOW"
 
-check_stock(0, "Cheese");
-# => "Cheese - OUT of stock!"
+# check_stock(0, "Cheese");
+# # => "Cheese - OUT of stock!"
 
-check_stock(1, "Salsa");
-# => "Salsa - running LOW"
+# check_stock(1, "Salsa");
+# # => "Salsa - running LOW"
